1c27217050b324f815af44c7a252cdfe05827daf
unknown <zaldyramlan24@gmail.com>
2024-06-15 00:38:38 +0700

    final fixing

--- laravel/tyas-astan/resources/views/pages/layout/header.blade.php
+++ laravel/tyas-astan/resources/views/pages/layout/header.blade.php
@@ -18,60 +18,6 @@
     <nav class="header-nav ms-auto">
         <ul class="d-flex align-items-center">
 
-            <li class="nav-item d-block d-lg-none">
-                <a class="nav-link nav-icon search-bar-toggle " href="#">
-                    <i class="bi bi-search"></i>
-                </a>
-            </li>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link nav-icon text-white" href="#" data-bs-toggle="dropdown">
-                    <i class="bi bi-bell"></i>
-                    <span class="position-absolute top-0 start-100 translate-middle p-1 mt-1 bg-danger rounded-circle">
-                        <span class="visually-hidden">New alerts</span>
-                    </span>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li class="notification-item">
-                        <div>
-                            <h4>Message</h4>
-                            <p>4 hrs. ago</p>
-                        </div>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                </ul>
-            </li>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link nav-icon text-white" href="#" data-bs-toggle="dropdown">
-                    <i class="bi bi-chat-left-text"></i>
-                    <span class="position-absolute top-0 start-100 translate-middle p-1 mt-1 bg-danger rounded-circle">
-                        <span class="visually-hidden">New alerts</span>
-                    </span>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li class="message-item">
-                        <a href="#">
-                            <div>
-                                <h4>Message</h4>
-                                <p>4 hrs. ago</p>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                </ul>
-            </li>
-
             <li class="nav-item dropdown pe-3">
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
                     <span
